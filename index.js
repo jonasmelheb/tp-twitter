d6dd5b36b3f1dbffa731c4617d84536e44eba4e3
Michael CORNILLON <contact@dwaps.fr>
2021-04-06 20:09:21 +0200

    servir fichier html et statiques

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const tabs = [
-  {
-    name: 'Accueil',
-    active: true,
-    link: '/home',
-  },
-  {
-    name: 'Contact',
-    active: false,
-    link: '/contact',
-  },
-];
+const indexHTMLFile = path.resolve('index.html');
 
+// Configuration de l'app Express
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(indexHTMLFile);
+})
 
 app.listen(3000);
