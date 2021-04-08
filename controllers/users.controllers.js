a6e35c635624598bce0027d0c661d2e6e0e9e0ad
Michael CORNILLON <contact@dwaps.fr>
2021-04-08 12:47:30 +0200

    refacto affichage utilisateurs

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,10 +3,10 @@ const User = require('../database/models/user.model');
 exports.usersPage = (req, res) => {
   User.find()
   .then(users => {
-    res.render('users-list.pug', { users });
+    res.render('users-list', { users });
   })
   .catch(error => {
     console.error(error);
-    res.render('users-list', { users: [] });
+    res.render('users-list');
   });
 }
